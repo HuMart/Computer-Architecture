de26628a106e1054a2b0b94e1f7492a1151569c9
HuMart <manya01@protonmail.com>
2019-10-17 18:52:19 -0400

    had to reformat to finish it

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,41 +8,48 @@ LDI = 0b10000010
 MUL = 0b10100010
 PUSH = 0b01000101
 POP = 0b01000110
+CALL = 0b01010000
+RET = 0b00010001
+ADD = 0b10100000
 
 
 class CPU:
     """Main CPU class."""
 
-    def __init__(self):
+    def __init__(self, file):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.reg = [8] * 8
+        self.reg = [0] * 8
         self.pc = 0
         self.sp = 0xF4
+        self.file = file
 
 
-    def ram_read(self, address):
-        return self.ram[address]
+    def ram_read(self, MAR):
+        return self.ram[MAR]
 
-    def ram_write(self, value, address):
-        self.ram[address] = value
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
+        address = 0
 
         try:
-            with open(sys.argv[1]) as f:
-                address = 0
+
+            with open(self.file) as f:
+                
                 for line in f:
                     line_split = line.split("#")
 
                     num = line_split[0].strip()
 
                     try:
-                        self.ram[address] = int(num, 2)
+                        val = int(num, 2)
+                        self.ram[address] = val
                         address += 1
                     except ValueError:
-                        pass
+                        continue
         
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {sys.argv[1]} Not Found")
@@ -89,37 +96,54 @@ class CPU:
         while running:
 
             IR = self.ram[self.pc]
-            op_a = self.ram_read(self.pc + 1)
-            op_b = self.ram_read(self.pc + 2)
             
             # LDI: load "immediate", store a value in a register, or "set this register to this value".
             if IR == LDI:
-                self.reg[op_a] = op_b
-                self.pc += 3
+                self.reg[self.ram[self.pc + 1]] = self.ram[self.pc + 2]
+                move = (IR >> 6) + 1
+                self.pc += move
             
             # PRN: a pseudo-instruction that prints the numeric value stored in a register.
             elif IR == PRN:
-                print(self.reg[op_a])
-                self.pc += 2
+                print(self.reg[self.ram[self.pc + 1]])
+                move = (IR >> 6) + 1
+                self.pc += move
 
             elif IR == MUL:
-                self.alu("MUL", op_a, op_b)
-                self.pc += 3
+                self.alu("MUL", self.ram[self.pc + 1], self.ram[self.pc + 2])
+                move = (IR >> 6) + 1
+                self.pc += move
+
+            elif IR == ADD:
+                self.alu("ADD", self.ram[self.pc + 1], self.ram[self.pc + 2])
+                move = (IR >> 6) + 1
+                self.pc += move
 
             elif IR == PUSH:
-                op_a = self.ram_read(self.pc + 1)
-                self.ram[self.sp] = self.reg[op_a]
                 self.sp -= 1
-                self.pc += 2
+                self.ram[self.sp] = self.reg[self.ram[self.pc + 1]]
+                move = (IR >> 6) + 1
+                self.pc += move
 
             elif IR == POP:
-                op_a = self.ram_read(self.pc + 1)
-                if self.sp == 0xF4:
-                    print("STACK IS EMPTY")
-                else:
-                    self.sp += 1
-                    self.reg[op_a] = self.ram[self.sp]
-                    self.pc += 2
-            
+                self.reg[self.ram[self.pc + 1]] = self.ram[self.sp]
+                self.sp += 1
+                move = (IR >> 6) + 1
+                self.pc += move
+
+            elif IR == CALL:
+                self.sp -= 1
+                self.ram[self.sp] = self.pc + 2
+                address = self.reg[self.ram[self.pc + 1]]
+                self.pc = address
+            elif IR == RET:
+                self.pc = self.ram[self.sp]
+                self.sp += 1
+
+                
+
             elif IR == HLT:
                 running = False
+
+            else:
+                sys.exit(1)
